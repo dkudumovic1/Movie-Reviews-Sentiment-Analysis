07913644492b923d2ed32f0c205f7635bdf1e404
lhodzic1 <lhodzic1@etf.unsa.ba>
2022-07-03 16:00:59 +0200

    count vectorizer update

--- preprocessing.py
+++ preprocessing.py
@@ -14,12 +14,13 @@ df = preprocessing.preprocess_reviews(df)
 df = preprocessing.remove_punctuation(df)
 #df.to_csv('IMDB dataset-preprocessed.csv', index = False, encoding='utf-8')
 
-#%%
-#TRAIN TEST SPLIT
-#1 - positive, 0 - negative
 Encoder = LabelEncoder()
 df['sentiment'] = Encoder.fit_transform(df['sentiment'])
 
+
+#%%
+#TRAIN TEST SPLIT
+#1 - positive, 0 - negative
 x_train, x_test, y_train, y_test = train_test_split(df['review'],df['sentiment'],test_size=0.15, shuffle=True)
 
 x_train.to_csv('x_train.csv', index = False, encoding='utf-8')
@@ -29,14 +30,15 @@ y_test.to_csv('y_test.csv', index = False, encoding='utf-8')
 
 # %%
 #COUNT VECTORIZER
+df = pd.read_csv('IMDB dataset-preprocessed.csv', converters = {'review': str})
 
-x_train = pd.read_csv('x_train.csv',converters = {'review': str})
-x_test = pd.read_csv('x_test.csv',converters = {'review': str})
+x_train = pd.read_csv('x_train.csv', converters = {'review': str})
+x_test = pd.read_csv('x_test.csv', converters = {'review': str})
 y_train = pd.read_csv('y_train.csv')
 y_test = pd.read_csv('y_test.csv')
 #posto je ovo ucitano ko df pri pozivanju modela ce se morat slat y_train['sentiment'], y_test['sentiment'] vjv
 
-dictionary, x_train_vector, x_test_vector = feature_extraction.get_count_vector(df, x_train['review'], x_test['review'])
+dictionary, x_train_vector, x_test_vector = feature_extraction.get_count_vector(df, x_train['review'], x_test['review'], remove_stopwords=False)
 #len(dictionary) - 27271
 #x_train_vector.shape - (42500, 27271)
 #x_test_vector.shape - (7500, 27271)
--- utils/feature_extraction.py
+++ utils/feature_extraction.py
@@ -1,9 +1,12 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
 #count vectorizer
-def get_count_vector(df, x_train, x_test):
+def get_count_vector(df, x_train, x_test, remove_stopwords=False):
 
     cv = CountVectorizer(min_df=0.0002)
+    if remove_stopwords:
+        cv.stop_words = 'english'
+        
     cv.fit(df['review'])
     x_train_vector = cv.transform(x_train)
     x_test_vector = cv.transform(x_test)
